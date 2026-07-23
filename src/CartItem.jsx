950469c36d6328e583a508e5fb47afd0787de4ad
vietlam2005 <dp1.1a2.lam@gmail.com>
2026-07-23 11:23:54 +0700

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,1 +1,18 @@
+import React from 'react';
+import './CartItem.css';
 
+function CartItem({ onContinueShopping }) {
+  return (
+    <div className="cart-container">
+      <h2>Shopping Cart</h2>
+      <div className="cart-items">
+        {/* Cart items list mapping goes here */}
+      </div>
+      <button className="get-started-button" onClick={onContinueShopping}>
+        Continue Shopping
+      </button>
+    </div>
+  );
+}
+
+export default CartItem;
